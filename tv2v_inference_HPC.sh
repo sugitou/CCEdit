41f6ddc46b5d33a703a265740b86cf6fd46c1769
sugitou <rs02358@surrey.ac.uk>
2025-08-11 15:33:17 +0100

    Try 15s video

--- tv2v_inference_HPC.sh
+++ tv2v_inference_HPC.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# export PYTHONPATH=/scratch/rs02358/ved_dissertation/CCEdit
-# export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
 export ACCELERATE_USE_DEEPSPEED=0
 export TRITON_CACHE_DIR=/parallel_scratch/rs02358/.triton/autotune
 export PYTHONPATH="/parallel_scratch/rs02358/CCEdit/src/taming-transformers:${PYTHONPATH}"
@@ -11,12 +9,12 @@ python scripts/sampling/sampling_tv2v.py \
     --ckpt_path models/tv2v-no2ndca-depthmidas.ckpt \
     --H 192 --W 256 \
     --original_fps 25 --target_fps 8 \
-    --num_keyframes 160 --batch_size 1 --num_samples 1 \
+    --num_keyframes 120 --batch_size 1 --num_samples 1 \
     --sample_steps 100 --sampler_name DPMPP2SAncestralSampler  --cfg_scale 12 \
     --prompt 'Two focused fencers in white uniforms duel on a strip inside a large sports hall' \
     --video_path /parallel_scratch/rs02358/Reference_Videos/v_Fencing_g01_c05.mp4 \
     --add_prompt 'anime style' \
-    --save_path outputs/tv2v/Fencing \
+    --save_path outputs/tv2v/Fencing-Longvideo \
     --disable_check_repeat \
     --prior_coefficient_x 0.3 \
     --basemodel_path models/base/revAnimated_v2Rebirth.safetensors \
